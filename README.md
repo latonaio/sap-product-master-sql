99af4be0eddbf8ecc089ee63ef83f4bf0e2f1cf8
Yurino Oga <yurino.o@latona.io>
2021-10-30 15:25:22 +0900

    release

---
+++ README.md
@@ -0,0 +1,12 @@
+# sap-product-master-sql  
+sap-product-master-sql は、主にエッジアプリケーションにおいて、SAPと連携された品目マスタデータを保存するSQLテーブルを作成するためのレポジトリです。
+
+## sqlの設定ファイル
+sap-product-master-sql には、sqlの設定ファイルとして以下のsqlファイルが含まれています。  
+
+* sap-product-master-sql-basic-data.sql  
+
+
+
+## MySQLのセットアップ / Kubernetesの設定 / SQLテーブルの作成方法
+MySQLのセットアップ / Kubernetesの設定 / 具体的なSQLテーブルの作成方法、については、mysql-kubeを参照ください。
---
+++ sap-product-master-sql-basic-data.sql
@@ -0,0 +1,15 @@
+CREATE TABLE `sap-product-master-basic-data`
+(
+    `Product`               varchar(40) DEFAULT NULL,
+    `Product_desc`          varchar(40) DEFAULT NULL,
+    `BaseUnit`              varchar(3) DEFAULT NULL,
+    `ValidityStartDate`     date DEFAULT NULL,
+    `ProductGroup`          varchar(9) DEFAULT NULL,
+    `Division`              varchar(2) DEFAULT NULL,
+    `GrossWeight`           float(13) DEFAULT NULL,
+    `WeightUnit`            varchar(3) DEFAULT NULL,
+    `SizeOrDimensionText`   varchar(32) DEFAULT NULL,
+    `ProductStandardID`     varchar(18) DEFAULT NULL,
+    PRIMARY KEY (`Product`),
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8;
