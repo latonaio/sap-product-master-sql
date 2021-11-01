0fe6d17515bd7ea7d510a9578df10ada787e572a
MiyuYokoyama0109 <miyu.y@latona.io>
2021-11-01 13:45:42 +0900

    release

--- sap-product-master-sql-work-scheduling-data (1).sql
+++ sap-product-master-sql-work-scheduling-data (1).sql
@@ -9,18 +9,6 @@ CREATE TABLE `sap-product-master-work-scheduling-data`
     `ProdnOrderIsBatchRequired` varchar(1) DEFAULT NULL,
     `MatlCompIsMarkedForBackflush` varchar(1) DEFAULT NULL,
     `ProductionSchedulingProfile` varchar(6) DEFAULT NULL,
-
-
-
-
-    `ProductDistributionChnl` varchar(2) DEFAULT NULL,
-    `SupplyingPlant`        varchar(4) DEFAULT NULL,
-    `PriceSpecificationProductGroup` varchar(2) DEFAULT NULL,
-    `AccountDetnProductGroup` varchar(2) DEFAULT NULL,
-    `ItemCategoryGroup`     varchar(4) DEFAULT NULL,
-    `SalesMeasureUnit`      varchar(3) DEFAULT NULL,
-    `IsMarkedForDeletion`   varchar(1) DEFAULT NULL,
-    `ProductHierarchy`      varchar(18) DEFAULT NULL,
     PRIMARY KEY (`Product`),
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8;
