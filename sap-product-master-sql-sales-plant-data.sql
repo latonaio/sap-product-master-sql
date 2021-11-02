637ca24ce0aecc96d9e371ae084717fe2a1af87f
Yurino Oga <yurino.o@latona.io>
2021-11-02 15:29:56 +0900

    release

--- sap-product-master-sql-sales-plant-data.sql
+++ sap-product-master-sql-sales-plant-data.sql
@@ -8,6 +8,7 @@ CREATE TABLE `sap-product-master-sales-plant-data`
     `TransportationGroup`   varchar(4) DEFAULT NULL,
     `LoadingGroup`          varchar(4) DEFAULT NULL,
     `AvailabilityCheckType` varchar(2) DEFAULT NULL,
-    PRIMARY KEY (`Product`),
+    PRIMARY KEY (`Product`, `Plant`),
+    CONSTRAINT `Product_fk` FOREIGN KEY (`Product`) REFERENCES `sap-product-master-basic-data` (`Product`)
 ) ENGINE = InnoDB
-  DEFAULT CHARSET = utf8;
+  DEFAULT CHARSET = utf8mb4;
