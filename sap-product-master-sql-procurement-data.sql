87fe304484aa5d311c3c317846833c106ecfb66a
Yurino Oga <yurino.o@latona.io>
2021-11-02 15:28:51 +0900

    release

--- sap-product-master-sql-procurement-data.sql
+++ sap-product-master-sql-procurement-data.sql
@@ -5,6 +5,7 @@ CREATE TABLE `sap-product-master-procurement-data`
     `PurchaseOrderQuantityUnit` varchar(3) DEFAULT NULL,
     `IsAutoPurOrdCreationAllowed` varchar(1) DEFAULT NULL,
     `IsSourceListRequired`  varchar(1) DEFAULT NULL,
-    PRIMARY KEY (`Product`),
+    PRIMARY KEY (`Product`, `Plant`),
+    CONSTRAINT `Product_fk` FOREIGN KEY (`Product`) REFERENCES `sap-product-master-basic-data` (`Product`)
 ) ENGINE = InnoDB
-  DEFAULT CHARSET = utf8;
+  DEFAULT CHARSET = utf8mb4;
