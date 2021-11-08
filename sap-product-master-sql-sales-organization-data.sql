1aae7b6707aba229ec4af145d1bddec95348ca32
Yurino Oga <yurino.o@latona.io>
2021-11-08 22:17:36 +0900

    release

--- sap-product-master-sql-sales-organization-data.sql
+++ sap-product-master-sql-sales-organization-data.sql
@@ -8,8 +8,8 @@ CREATE TABLE `sap-product-master-sales-organization-data`
     `AccountDetnProductGroup` varchar(2) DEFAULT NULL,
     `ItemCategoryGroup`     varchar(4) DEFAULT NULL,
     `SalesMeasureUnit`      varchar(3) DEFAULT NULL,
-    `IsMarkedForDeletion`   varchar(1) DEFAULT NULL,
     `ProductHierarchy`      varchar(18) DEFAULT NULL,
+    `IsMarkedForDeletion`   varchar(1) DEFAULT NULL,
     PRIMARY KEY (`Product`, `ProductSalesOrg`, `ProductDistributionChnl`),
     CONSTRAINT `Product_fk` FOREIGN KEY (`Product`) REFERENCES `sap-product-master-basic-data` (`Product`)
 ) ENGINE = InnoDB
