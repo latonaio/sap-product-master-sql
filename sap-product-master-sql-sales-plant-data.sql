8e796fceba40ab751d469b752f85de97732c6819
Yurino Oga <yurino.o@latona.io>
2021-12-01 14:40:29 +0900

    release

--- sap-product-master-sql-sales-plant-data.sql
+++ sap-product-master-sql-sales-plant-data.sql
@@ -2,10 +2,6 @@ CREATE TABLE `sap-product-master-sales-plant-data`
 (
     `Product`                 varchar(40) DEFAULT NULL,
     `Plant`                   varchar(4) DEFAULT NULL,
-    `SalesStatus`             varchar(2) DEFAULT NULL,
-    `SalesStatusValidityDate` date DEFAULT NULL,
-    `TaxClassification`       varchar(1) DEFAULT NULL,
-    `TransportationGroup`     varchar(4) DEFAULT NULL,
     `LoadingGroup`            varchar(4) DEFAULT NULL,
     `AvailabilityCheckType`   varchar(2) DEFAULT NULL,
     PRIMARY KEY (`Product`, `Plant`),
